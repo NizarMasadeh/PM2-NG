a48af025ab269e389004209420c1a8d4a788352c
Nizar Masadeh <nizarmasadeh2001@gmail.com>
2025-03-14 22:41:26 +0300

    Swagger added to backend on /

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,41 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  app.enableCors()
-  app.useGlobalPipes(new ValidationPipe())
-  app.setGlobalPrefix("api")
+  const app = await NestFactory.create(AppModule);
 
-  const port = process.env.PORT || 3000
-  await app.listen(port)
-  console.log(`Server running on port ${port}`)
+  const config = new DocumentBuilder()
+    .setTitle('PM2-NG Backend')
+    .setDescription('The swagger doc')
+    .setVersion('1.0')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      name: 'JWT',
+      description: 'Enter JWT token',
+      in: 'header',
+    },
+      'access-token',
+    )
+    .build();
+
+  app.setGlobalPrefix("api", {
+    exclude: ['/'],
+  });
+
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+
+  const document = SwaggerModule.createDocument(app, config);
+  
+  SwaggerModule.setup('', app, document);
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Server running on port ${port}`);
+  console.log(`Swagger documentation available at http://localhost:${port}`);
 }
-bootstrap()
+bootstrap();
