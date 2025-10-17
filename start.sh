651ca1668ee3ef434ccab38af6aad286790a3831
NZRM VPS <me@nizar.io>
2025-10-17 23:10:12 +0200

    sh script

--- start.sh
+++ start.sh
@@ -12,23 +12,25 @@ NC='\033[0m'
 FRONTEND_PATH="$(pwd)/dist/browser"
 BACKEND_PATH="$(pwd)/backend"
 
-echo -e "${YELLOW}=== Pulling from git ===${NC}"
+echo -e "${YELLOW}=== Pulling from git ==="
+git swtich main
 git pull origin main
 
-echo -e "${YELLOW}Installing Dependencies...${NC}"
+echo -e "${YELLOW}Installing Dependencies..."
 npm install
 npm --prefix "$BACKEND_PATH" install
 
-echo -e "${YELLOW}Building Frontend & Backend...${NC}"
+echo -e "${YELLOW}Building Frontend & Backend..."
 npm run build:frontend:prod
 npm --prefix "$BACKEND_PATH" run build
+npm install --omit=dev && npm --prefix "$BACKEND_PATH" install --omit=dev
 
-echo -e "${YELLOW}Running Backend...${NC}"
+echo -e "${YELLOW}Running Backend..."
 pm2 reload pm2-ng-backend || pm2 start "$BACKEND_PATH/dist/main.js" --name pm2-ng-backend
 
-echo -e "${YELLOW}Running Frontend...${NC}"
+echo -e "${YELLOW}Running Frontend..."
 pm2 reload pm2-ng-frontend || pm2 serve "$FRONTEND_PATH" 4141 --name pm2-ng-frontend --spa
 
-echo -e "${GREEN}Deployed Successfully!${NC}"
-echo -e "${GREEN}Backend running with PM2 as 'pm2-ng-backend'${NC}"
-echo -e "${GREEN}Frontend running at: http://localhost:4141${NC}"
+echo -e "${GREEN}Deployed Successfully!"
+echo -e "${GREEN}Backend running with PM2 as 'pm2-ng-backend'"
+echo -e "${GREEN}Frontend running at: http://localhost:4141"
